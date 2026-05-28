8d45578d3ed09f46c9be56f492777e5057aeac17
Adarsh Gouda <adhi.pesit@gmail.com>
2026-05-28 01:07:20 -0500

    Add rudra_core ROS2 heartbeat package

---
+++ ros2_ws/src/rudra_core/rudra_core/rudra_alive.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print('Hi from rudra_core.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_ws/src/rudra_core/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'rudra_core'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Adarsh Gouda',
+    maintainer_email='adhi.pesit@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'rudra_alive = rudra_core.rudra_alive:main'
+        ],
+    },
+)
