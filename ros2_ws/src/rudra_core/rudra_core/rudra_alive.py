623bd6d0a23a78b0f4417147ab108849f4bac595
Adarsh Gouda <adhi.pesit@gmail.com>
2026-05-28 01:37:46 -0500

    Codex rudra_alinve.py

--- ros2_ws/src/rudra_core/rudra_core/rudra_alive.py
+++ ros2_ws/src/rudra_core/rudra_core/rudra_alive.py
@@ -1,5 +1,35 @@
-def main() -> None:
-    print('Hi from rudra_core.')
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+
+
+class RudraAlive(Node):
+    def __init__(self) -> None:
+        super().__init__('rudra_alive')
+        self.heartbeat = 0
+        self.heartbeat_pub = self.create_publisher(Int32, 'rudra/heartbeat', 10)
+        self.get_logger().info('RUDRA alive node started.')
+        self.timer = self.create_timer(1.0, self.publish_heartbeat)
+
+    def publish_heartbeat(self) -> None:
+        msg = Int32()
+        msg.data = self.heartbeat
+        self.heartbeat_pub.publish(msg)
+        self.get_logger().info(f'RUDRA is alive. Heartbeat = {self.heartbeat}')
+        self.heartbeat += 1
+
+
+def main(args: list[str] | None = None) -> None:
+    rclpy.init(args=args)
+    node = RudraAlive()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
